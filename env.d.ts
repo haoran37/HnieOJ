05ec4041c2efc7e1a1d19b4fddd3bb3029f9a842
_37_ <1360990035@qq.com>
2026-01-07 14:58:33 +0800

    引入并配置v-md-editor、prismjs和katex

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,26 @@
 /// <reference types="vite/client" />
+
+// src/v-md-editor.d.ts
+declare module '@kangc/v-md-editor/lib/preview' {
+  import type { DefineComponent } from 'vue';
+
+  const VMdPreview: DefineComponent<{}, {}, any> & {
+    use(theme: any, config?: any): void;
+    install(app: any): void;
+  };
+
+  export default VMdPreview;
+}
+
+declare module '@kangc/v-md-editor/lib/theme/github.js' {
+  const theme: any;
+  export default theme;
+}
+
+declare module '@kangc/v-md-editor/lib/plugins/katex/cdn' {
+  const createKatexPlugin: () => any;
+  export default createKatexPlugin;
+}
+
+declare module '@kangc/v-md-editor/lib/plugins/line-number/index';
+declare module '@kangc/v-md-editor/lib/plugins/copy-code/index';
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,30 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+// v-md-editor
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
+import '@kangc/v-md-editor/lib/style/preview.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+// 高亮支持
+import Prism from 'prismjs';
+// 数学公式
+import createKatexPlugin from '@kangc/v-md-editor/lib/plugins/katex/cdn';
 // 通用字体
 import 'vfonts/Lato.css'
 // 等宽字体
 import 'vfonts/FiraCode.css'
 
+VMdPreview.use(githubTheme, {
+  Prism,
+});
+
+VMdPreview.use(createKatexPlugin());
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VMdPreview);
 
 app.mount('#app')
